6d3a862fdb317b2ef1ba12e0487aa54c05889338
rishavemail786 <rishavemail786@gmail.com>
2022-08-11 15:00:32 +0530

    SOLUTION DONE

--- src/main/java/org/example/Stock.java
+++ src/main/java/org/example/Stock.java
@@ -1,2 +1,20 @@
-package org.example;public class Stock {
+package org.example;
+
+public class Stock {
+    String stockSymbol;
+    String stockName;
+    double previousClosingPrice;
+    double currentPrice;
+
+    public Stock(String stockSymbol, String stockName, double previousClosingPrice, double currentPrice) {
+        this.stockSymbol = stockSymbol;
+        this.stockName = stockName;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+
+    }
+    public double getChangePercent(){
+        double changePercent= (currentPrice-previousClosingPrice)/previousClosingPrice*100;
+        return changePercent;
+    }
 }
--- src/main/java/org/example/StockIMPL.java
+++ src/main/java/org/example/StockIMPL.java
@@ -1,7 +1,13 @@
 package org.example;
 
-public class Main {
+public class StockIMPL {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stock stock1 = new Stock("ORCL","Oracle Corporation",34.5,34.35);
+        System.out.println("Stock symbol name    = "+stock1.stockSymbol);
+        System.out.println("Stock  name          = "+stock1.stockName);
+        System.out.println("Stock previous price = "+stock1.previousClosingPrice);
+        System.out.println("Stock current  price = "+stock1.currentPrice);
+        System.out.println("stock change percent = "+stock1.getChangePercent());
+
     }
 }
